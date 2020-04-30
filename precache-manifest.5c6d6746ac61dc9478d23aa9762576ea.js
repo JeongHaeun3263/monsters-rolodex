e52c696f6f1157fcd5e0bd94a6e801c0bf53b4ff
JeongHaeun3263 <jhe3263@gmail.com>
2020-04-30 13:48:46 -0400

    Updates

---
+++ precache-manifest.5c6d6746ac61dc9478d23aa9762576ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98427060f994ed59bec65d58c8aca06",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f245302bffb109918c08",
+    "url": "/monsters-rolodex/static/css/main.c5ce5081.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f245302bffb109918c08",
+    "url": "/monsters-rolodex/static/js/main.a9af1875.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
